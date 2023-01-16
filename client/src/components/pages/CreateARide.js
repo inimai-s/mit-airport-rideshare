e34284cacf34094d7bed6e0d9f576e6830576953
Audrey Ha <audreyha@gmail.com>
2023-01-16 14:03:45 -0500

    Basic form for Destination and Your MIT Location

--- client/src/components/pages/CreateARide.js
+++ client/src/components/pages/CreateARide.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 
 // import { get } from "../../utilities";
 
-const CreateARide = () => {
+const CreateARide = (props) => {
   // const [stories, setStories] = useState([]);
 
   // // called when the "Feed" component "mounts", i.e.
@@ -37,11 +37,55 @@ const CreateARide = () => {
   // } else {
   //   storiesList = <div>No stories!</div>;
   // }
+
+  // Radio buttons for MIT vs. Logan Airport destination
+  const [destinationMIT, setDestinationMIT]=useState(false);
+  const [destinationLogan, setDestinationLogan]=useState(false);
+
+  const handleDestinationMITChange = (event) => {
+      const value=event.target.checked;
+      setDestinationMIT(value)
+  };
+
+  const handleDestinationLoganChange = (event) => {
+    const value=event.target.checked;
+    setDestinationLogan(value)
+  };
+
+  // Your MIT Location
+  const [mitLocationText,setMitLocationText]=useState("");
+
+  const handleMitLocationChange = (event) => {
+      const value=event.target.value;
+      setMitLocationText(value);
+  };
+
+  // Submit button
+  const submitRide = () => {
+    // Send stuff to Mongo
+
+    // Clear the form
+    setMitLocationText("");
+    setDestinationMIT(false);
+    setDestinationLogan(false);
+  };
+
   return (
     <>
       {/* <NewStory addNewStory={addNewStory} />
       {storiesList} */}
       <h1>Create A Ride</h1>
+      <h4>You will be the "Captain" for this shared ride, and others can join on the "Find a Ride" page.</h4>
+      <div>
+        <h4>Destination</h4>
+        <input type="radio" name="destination" value="mit" checked={destinationMIT} onChange={handleDestinationMITChange}/> MIT
+        <input type="radio" name="destination" value="loganAirport" checked={destinationLogan} onChange={handleDestinationLoganChange}/>Logan Airport
+
+        <h4>Your MIT Location</h4>
+        <input type="text" value={mitLocationText} onChange={handleMitLocationChange} />
+        
+        <button onClick={submitRide}>Submit!</button>
+      </div>
     </>
   );
 };
