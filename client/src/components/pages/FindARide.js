273176d395de1f0112d094c21bafea8ba51fd493
Audrey Ha <audreyha@gmail.com>
2023-01-18 16:15:16 -0500

    Added basic modal

--- client/src/components/pages/FindARide.js
+++ client/src/components/pages/FindARide.js
@@ -4,6 +4,9 @@ import Card from "../modules/Card.js";
 import { get } from "../../utilities";
 import { post } from "../../utilities";
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 const FindARide = (props) => {
   const [rides, setRides] = useState([]);
 
@@ -38,10 +41,35 @@ const FindARide = (props) => {
     ridesList = <div>No rides!</div>;
   }
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
   let masterModal = null;
   if (props.user_googleid && props.user_name){
     masterModal=<>
       <h1>Find A Ride</h1>
+      
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       {ridesList}
     </>
   }else{
