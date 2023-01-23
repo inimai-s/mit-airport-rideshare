9520cdd8f73b39f25bbbed73d87ff0e315166320
Audrey Ha <audreyha@gmail.com>
2023-01-23 13:37:34 -0500

    Updated getJoinedRides to getActiveJoinedRides in Chatbook.js

--- client/src/components/pages/Chatbook.js
+++ client/src/components/pages/Chatbook.js
@@ -65,7 +65,7 @@ const Chatbook = (props) => {
       my_googleid: props.user_googleid,
     };
 
-    get("/api/getJoinedRides", query).then((rideObjs) => {
+    get("/api/getActiveJoinedRides", query).then((rideObjs) => {
       // console.log("getting Joined Rides list");
       // console.log(`rideObjs: ${rideObjs}`);
       // console.log(`props.user_googleid for Chatbook.js: ${props.user_googleid}`);
