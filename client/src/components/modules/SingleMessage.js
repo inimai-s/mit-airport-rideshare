deabf911f9948e0d6c69cd2d1ffdd2df03b864a2
Audrey Ha <audreyha@gmail.com>
2023-01-27 13:28:35 -0500

    Links are clickable in the chat

--- client/src/components/modules/SingleMessage.js
+++ client/src/components/modules/SingleMessage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import "./SingleMessage.css";
 
+import Linkify from 'react-linkify';
+
 /**
  * Renders a single chat message
  *
@@ -9,6 +11,7 @@ import "./SingleMessage.css";
  * @param {MessageObject} message
  */
 const SingleMessage = (props) => {
+
   let messageModal=<></>
 
   console.log(`props.message.sender._id ${props.message.sender._id}`)
@@ -17,14 +20,31 @@ const SingleMessage = (props) => {
   if (props.message.sender._id === props.userId){
     messageModal=<>
     <div className={"u-flex u-flex-alignCenter SingleMessage-container"}>
-      <span className="SingleMessage-myContent">{props.message.content}</span>
+      <span className="SingleMessage-myContent">
+        <Linkify componentDecorator={(decoratedHref, decoratedText, key) => (
+          <a target="blank" href={decoratedHref} key={key} className="SingleMessage-myLink">
+              {decoratedText}
+          </a>
+        )}>
+            {props.message.content}
+        </Linkify>
+      </span>
     </div>
     </>
   }else{
     messageModal=<>
     <div className={"u-flex u-flex-alignCenter SingleMessage-container"}>
       <span className="SingleMessage-sender u-bold">{props.message.sender.user_name + ":"}</span>
-      <span className="SingleMessage-content">{props.message.content}</span>
+      
+      <span className="SingleMessage-content">
+        <Linkify componentDecorator={(decoratedHref, decoratedText, key) => (
+          <a target="blank" href={decoratedHref} key={key}>
+              {decoratedText}
+          </a>
+        )}>
+            {props.message.content}
+        </Linkify>
+      </span>
     </div>
     </>
   }
