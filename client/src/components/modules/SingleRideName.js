11ace29028e38214a88a0b0627b0647503b62dfc
Audrey Ha <audreyha@gmail.com>
2023-01-28 17:53:49 -0500

    Profile icons show up next to chat names

--- client/src/components/modules/SingleRideName.js
+++ client/src/components/modules/SingleRideName.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from "react";
 
+import "./Card.css";
+
 import "./SingleRideName.css";
+
+import { get } from "../../utilities";
+import { post } from "../../utilities";
+
+import { Link } from "@reach/router";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 /**
  * Component to render an online user
@@ -12,6 +26,26 @@ import "./SingleRideName.css";
  * @param {boolean} active
  */
 const SingleRideName = (props) => {
+  const [members,setMembers] = useState([]);
+  const handleMembers=(val)=>{
+    setMembers(old => {return [...old, val]});
+  };
+
+  useEffect(() => {
+    get("/api/getOneRide",{_id:props.rideJoined._id}).then((ride) => {
+      for (var i = 0; i<ride.user_googleId_joined.length; i++) {
+        if(i===0){
+          setMembers([]);
+        }
+
+        get("/api/getName2",{user_googleid:ride.user_googleId_joined[i]}).then((user) => {
+          handleMembers({photoLink:user.photoLink,
+          });
+        })
+      }
+    })
+  },[props.rideJoined._id]);
+
   return (
     <div
       className={`SingleRideName-container u-pointer ${props.active ?
@@ -22,6 +56,10 @@ const SingleRideName = (props) => {
       }}
     >
       {props.rideJoined.user_name}'s Ride to {props.rideJoined.destination}, {props.rideJoined.start_date} 
+      <br></br>
+      {members.map((m,i) => (
+        <img key={i} src={m.photoLink} alt="Profile Image" className="SingleRideName-profilePhoto"/>
+      ))}
     </div>
   );
 }
